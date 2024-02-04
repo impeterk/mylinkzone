8a86772b0eb5a00a9b82d3196e591a65bcb256c3
Peter <storm.sk123@gmail.com>
2024-02-04 23:49:38 +0100

    Initial commit
    Add Nuxt UI, Nuxt Icon, basic background, vuefire(firebase), nuxt Image

---
+++ app.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+
+</template>
+<style>
+body, html, #__nuxt {
+  @apply min-h-dvh bg-gradient-to-t from-slate-300 to-slate-100 dark:from-slate-950 dark:to-slate-900;
+}
+
+</style>
